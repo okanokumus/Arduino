c3d468265f6d3a0bcb3940c624ab809bb487c1b3
Okan Okumus <okanokumuss@gmail.com>
2017-05-29 23:52:56 +0300

    PWM

---
+++ Pulse Width Modulation/PWM_uygulama/PWM_uygulama.ino
@@ -0,0 +1,24 @@
+int led = 9; // the pin that the LED is attached to
+int brightness = 0; // how bright the LED is
+int fadeamount = 5; // how many points to fade the LED by
+// the setup routine runs once when you press reset
+void setup() {
+  // put your setup code here, to run once:
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+}
+
+//the loop routine runs over and over again forever:
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // set the brightness of pin 9:
+  analogWrite(led, brightness); 
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeamount;
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness==0 || brightness ==255){
+    fadeamount = -fadeamount;
+  }
+  delay (30);
+}
